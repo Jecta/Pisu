875919f9a01f6c0d50319ade17f57535251f54f9
jecta <jectaunscripted@gmail.com>
2022-01-15 19:12:49 +0100

    Upload from local

---
+++ PisuRunner.js
@@ -0,0 +1,48 @@
+import color from "chalk";
+import readline from "readline";
+import { base64encode, base64decode } from "nodejs-base64"
+
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function question(string) {
+    return new Promise((res) => {
+        rl.question(string, (answer) => res(answer))
+    })
+}
+
+async function Pisu() {
+
+    console.clear();
+
+    console.log("\n")
+
+    console.log(color.red("\n" +
+        " ██▓███   ██▓  ██████  █    ██ \n" +
+        "▓██░  ██▒▓██▒▒██    ▒  ██  ▓██▒\n" +
+        "▓██░ ██▓▒▒██▒░ ▓██▄   ▓██  ▒██░\n" +
+        "▒██▄█▓▒ ▒░██░  ▒   ██▒▓▓█  ░██░\n" +
+        "▒██▒ ░  ░░██░▒██████▒▒▒▒█████▓ \n" +
+        "▒▓▒░ ░  ░░▓  ▒ ▒▓▒ ▒ ░░▒▓▒ ▒ ▒ \n" +
+        "░▒ ░      ▒ ░░ ░▒  ░ ░░░▒░ ░ ░ \n" +
+        "░░        ▒ ░░  ░  ░   ░░░ ░ ░ \n" +
+        "          ░        ░     ░     \n" +
+        "                               \n"));
+
+    question(color.blue('Please enter the userID: ')).then(async Results => {
+        await Results
+
+        if(isNaN(Results)) {
+            return console.log(color.white("There are only numbers in a userID."))
+        } else {
+            let tokenpart = base64encode(Results);
+
+            console.log(color.red("First Token Part:" + tokenpart));
+
+        }
+
+    })
+}
+ Pisu()
